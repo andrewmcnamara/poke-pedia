3a3fed461900fafd066c389c3aa3bf699a75eaab
Andrew McNamara <andrew.mcnamara@bugcrowd.com>
2022-10-23 13:32:48 +1100

    add more config

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -1,8 +1,13 @@
 import * as trpcNext from "@trpc/server/adapters/next";
-import { appRouter } from "@/backend/routers/_app";
+import { appRouter, type AppRouter } from "@/backend/routers/_app";
+import { inferProcedureOutput } from "@trpc/server";
 
 // export API handler
 export default trpcNext.createNextApiHandler({
   router: appRouter,
   createContext: () => ({}),
 });
+
+export type inferQueryResponse<
+  TRouteKey extends keyof AppRouter["_def"]["procedures"]
+> = inferProcedureOutput<AppRouter["_def"]["procedures"][TRouteKey]>;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,36 @@
 import type { NextPage } from "next";
 import { trpc } from "../utils/trpc";
+import { inferQueryResponse } from "./api/trpc/[trpc]";
 
 export default function Home() {
   const hello = trpc.hello.useQuery({ text: "client" });
+
   if (!hello.data) {
     return <div>Loading...</div>;
   }
 
   return (
-    <div className="h-screen w-screen flex flex-col justify-center">
-      <div className="border rounded">{hello.data.greeting}</div>
-      <img
-        src={hello.data.pokemon.sprites.front_default}
-        alt={hello.data.pokemon.name}
-      />
+    <div className="h-screen w-screen flex flex-col justify-center items-center">
+      <div className="border flex rounded max-w-2xl">
+        <PokemonCard pokemon={hello.data.pokemon} />
+      </div>
     </div>
   );
 }
+
+type PokemonFromServer = inferQueryResponse<"hello">["pokemon"];
+
+const PokemonCard: React.FC<{ pokemon: PokemonFromServer }> = ({ pokemon }) => {
+  return (
+    <div className="w-64 h-64 flex flex-col">
+      <img
+        src={pokemon.sprites.front_default}
+        alt={pokemon.name}
+        className="w-full"
+      />
+      <div className="text-xl text-center capitalize mt-[-4rem]">
+        {pokemon.name}
+      </div>
+    </div>
+  );
+};
